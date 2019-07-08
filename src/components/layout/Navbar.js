baeeadd4798fa32ae2b76d396aeee4f414f90e07
alaakanaan <alaa.kanaan@syndigate.info>
2019-07-08 16:54:32 +0300

    add navbar and style

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from "react-router-dom";
 
 const Navbar = ({title, icon}) => {
 
     return (
-        <div className="navbar bg-contact-keeper text-white">
-            <h4><i className={icon}/> {title}</h4>
-
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-contact-keeper">
+            <Link to='/' className="navbar-brand text-white"><i className={icon}/> {title}</Link>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <Link to='/' className="nav-link">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to='/about' className="nav-link">About</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
 
 };
