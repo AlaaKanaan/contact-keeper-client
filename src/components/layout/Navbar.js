7bc0bd8cc4f7dec3ed30690ebbe6d2ca710fd8d8
alaakanaan <alaa.kanaan@syndigate.info>
2019-07-10 17:01:43 +0300

    add navbar and style

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,19 +7,21 @@ const Navbar = ({title, icon}) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-contact-keeper">
             <Link to='/' className="navbar-brand text-white"><i className={icon}/> {title}</Link>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <Link to='/' className="nav-link">Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to='/about' className="nav-link">About</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+            <button className="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link to='/' className="nav-link">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/about' className="nav-link">About</Link>
+                    </li>
+                </ul>
             </div>
         </nav>
     );
