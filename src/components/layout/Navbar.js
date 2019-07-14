9eccc9c120c0f615f6e6d4d73a4d41d3198477e3
alaakanaan <alaa.kanaan@syndigate.info>
2019-07-14 16:23:36 +0300

    add navbar and style

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = ({title, icon}) => {
             <button className="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                <span className="navbar-toggler-icon"/>
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
