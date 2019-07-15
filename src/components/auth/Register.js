57705bf0a3a3ae22f74ffa514642c067b28c39b5
alaakanaan <alaa.kanaan@syndigate.info>
2019-07-15 10:53:22 +0300

    add navbar and style

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import AlertContext from '../../context/alert/alertContext';
 import AuthContext from '../../context/auth/authContext';
 
@@ -8,7 +8,14 @@ const Register = () => {
     const authContext = useContext(AuthContext);
 
     const {setAlert} = alertContext;
-    const {register} = authContext;
+    const {register, error, clearErrors} = authContext;
+
+    useEffect(() => {
+        if (error) {
+            setAlert(error, 'danger');
+            clearErrors();
+        }
+    }, [error]);
 
     const [user, setUser] = useState({
         name: '',
