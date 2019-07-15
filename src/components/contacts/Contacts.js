10a0f16ee8ad4daf00d62007fbc0f6ef48319fe1
alaakanaan <alaa.kanaan@syndigate.info>
2019-07-15 15:59:16 +0300

    add navbar and style

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -2,6 +2,7 @@ import React, {Fragment, useContext, useEffect} from 'react';
 import {CSSTransition, TransitionGroup} from "react-transition-group";
 import ContactContext from '../../context/contact/contactContext'
 import ContactItem from './ContactItem';
+import Spinner from '../layout/Spinner';
 import './assets/contacts.css';
 
 const Contacts = props => {
@@ -10,7 +11,7 @@ const Contacts = props => {
     useEffect(() => {
         getContacts();
         //eslint-disable-next-line
-    },[]);
+    }, []);
     const {contacts, filteredContacts, getContacts, loading} = contactContext;
 
     if (contacts.length === 0) {
@@ -23,13 +24,16 @@ const Contacts = props => {
 
     return (
         <Fragment>
-            <TransitionGroup>
-                {contactsList.map(contact => (
-                    <CSSTransition key={contact._id} timeout={300} classNames="item">
-                        <ContactItem key={contact._id} contact={contact}/>
-                    </CSSTransition>
-                ))}
-            </TransitionGroup>
+            {!loading ? (
+                <TransitionGroup>
+                    {contactsList.map(contact => (
+                        <CSSTransition key={contact._id} timeout={300} classNames="item">
+                            <ContactItem key={contact._id} contact={contact}/>
+                        </CSSTransition>
+                    ))}
+                </TransitionGroup>
+            ) : <Spinner/>}
+
         </Fragment>
     );
 };
