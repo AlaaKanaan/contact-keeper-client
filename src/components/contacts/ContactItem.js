47e373cb5cd9bdeed661b300bc50a455eb1fe62b
alaakanaan <alaa.kanaan@syndigate.info>
2019-07-15 16:26:54 +0300

    add navbar and style

--- src/components/contacts/ContactItem.js
+++ src/components/contacts/ContactItem.js
@@ -8,10 +8,10 @@ const ContactItem = ({contact}) => {
     const contactContext = useContext(ContactContext);
     const {deleteContact, setCurrentContact, clearCurrentContact} = contactContext;
 
-    const {id, name, email, phone, type} = contact;
+    const {_id, name, email, phone, type} = contact;
 
     const onDelete = () => {
-        deleteContact(id);
+        deleteContact(_id);
         clearCurrentContact();
     };
 
